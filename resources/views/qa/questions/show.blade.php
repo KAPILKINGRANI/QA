5d8b9833e9324bb249a65445f350788422ded6eb
Kapil <kingranikapil3@gmail.com>
2024-06-05 12:11:20 +0530

    feat:Display of upvotes,downvotes,favourite on question page

--- resources/views/qa/questions/show.blade.php
+++ resources/views/qa/questions/show.blade.php
@@ -1,4 +1,7 @@
 @extends('qa.layouts.app')
+@section('page-level-scripts')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+@endsection
 @section('content')
     <div class="container">
         <div class="row">
@@ -12,7 +15,25 @@
                     </div>
                     <div class="card-footer">
                         <div class="d-flex justify-content-between mr-3">
-                            <div></div>
+                            <div>
+                                <div class="d-flex">
+                                    <div>
+                                        <a href="#" title="Up Vote" class="vote-up d-block text-center text-dark">
+                                            <i class="fa fa-caret-up fa-3x"></i>
+                                        </a>
+                                        <h4 class="votes-count textmuted text-center m-0">{{ $question->votes_count }} </h4>
+                                        <a href="#" title="Down Vote" class="vote-up d-block text-center text-dark">
+                                            <i class="fa fa-caret-down fa-3x"></i>
+                                        </a>
+                                    </div>
+                                    <div class="ml-4 mt-2">
+                                        <a href="#" title="Mark as Fav">
+                                            <i class="fa fa-star fa-2x text-dark"></i>
+                                        </a>
+                                        <h4>123</h4>
+                                    </div>
+                                </div>
+                            </div>
                             <div class="d-flex flex-column">
                                 <div class="text-muted mb-2 text-right">
                                     Asked At : {{ $question->created_date }}
@@ -43,7 +64,27 @@
                         @foreach ($question->answers as $answer)
                             {!! $answer->body !!}
                             <div class="d-flex justify-content-between mr-3">
-                                <div></div>
+                                <div>
+                                    <div class="d-flex">
+                                        <div>
+                                            <a href="#" title="Up Vote" class="vote-up d-block text-center text-dark">
+                                                <i class="fa fa-caret-up fa-3x"></i>
+                                            </a>
+                                            <h4 class="votes-count textmuted text-center m-0">{{ $answer->votes_count }}
+                                            </h4>
+                                            <a href="#" title="Down Vote"
+                                                class="vote-up d-block text-center text-dark">
+                                                <i class="fa fa-caret-down fa-3x"></i>
+                                            </a>
+                                        </div>
+                                        <div class="ml-4 mt-2">
+                                            <a href="#" title="Mark as Fav">
+                                                <i class="fa fa-star fa-2x text-dark"></i>
+                                            </a>
+                                            <h4>123</h4>
+                                        </div>
+                                    </div>
+                                </div>
                                 <div class="d-flex flex-column">
                                     <div class="text-muted mb-2 text-right">
                                         Answered At : {{ $answer->created_date }}
